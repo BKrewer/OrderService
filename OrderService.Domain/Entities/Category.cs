3b3cffd5739fc365a97aa09fd5e95d8be0d1aad3
bkrewer <brnkrewer@gmail.com>
2021-09-20 20:40:24 -0300

    Adicionar arquivos de projeto.

---
+++ OrderService.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderService.Domain.Entities
+{
+    public class Category
+    {
+        public Category(string name)
+        {
+            Name = name;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+    }
+}
---
+++ OrderService.Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace OrderService.Domain.Entities
+{
+    public class Product
+    {
+        public Product(string name, string description, decimal price, int quantityInStock, string image)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+            QuantityInStock = quantityInStock;
+            Image = image;
+        }
+
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int QuantityInStock { get; private set; }
+
+        public string Image { get; private set; }
+    }
+}
